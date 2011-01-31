2ac0d8838d3f65b1ab9b3af2480f542608ed3742
Timothy Redmond <tredmond@stanford.edu>
2011-01-31 15:19:35 +0000

    the jlist still isn't selectable.

--- src/main/java/org/protege/editor/owl/diff/DifferenceList.java
+++ src/main/java/org/protege/editor/owl/diff/DifferenceList.java
@@ -1,16 +1,19 @@
 package org.protege.editor.owl.diff;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.FlowLayout;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.JComponent;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.owl.diff.present.Changes;
@@ -24,10 +27,10 @@ public class DifferenceList extends JPanel {
 	private JList entityBasedDiffList;
 
 	public DifferenceList(OWLModelManager manager) {
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		setLayout(new BorderLayout());
 		this.diffs = DifferenceConfiguration.get(manager);
-		add(createDifferenceListComponent());
-		add(createDifferenceTable());
+		add(createDifferenceListComponent(), BorderLayout.WEST);
+		add(createDifferenceTable(), BorderLayout.CENTER);
 		if (diffs.isReady()) {
 			fillEntityBasedDiffList();
 		}
@@ -51,9 +54,20 @@ public class DifferenceList extends JPanel {
 	}
 	
 	private JComponent createDifferenceListComponent() {
-		entityBasedDiffList = new JList(new DefaultListModel());
+		entityBasedDiffList = new JList();
+		entityBasedDiffList.setModel(new DefaultListModel());
+		entityBasedDiffList.setSelectionModel(new DefaultListSelectionModel());
 		entityBasedDiffList.setCellRenderer(new EntityBasedDiffRenderer());
 		entityBasedDiffList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		entityBasedDiffList.addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				Object o = entityBasedDiffList.getSelectedValue();
+				if (o instanceof EntityBasedDiff) {
+					diffs.setSelection((EntityBasedDiff) o);
+				}
+			}
+		});
 		JScrollPane pane = new JScrollPane(entityBasedDiffList);
 		return pane;
 	}
