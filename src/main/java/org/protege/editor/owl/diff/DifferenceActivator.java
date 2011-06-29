09c0b546ca1ae35f95a2dc22669e6547349d0c56
Timothy Redmond <tredmond@stanford.edu>
2011-06-29 18:31:37 +0000

    create the compare ontology views category for the View menu

---
+++ src/main/java/org/protege/editor/owl/diff/DifferenceActivator.java
@@ -0,0 +1,16 @@
+package org.protege.editor.owl.diff;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.protege.editor.core.ProtegeProperties;
+
+public class DifferenceActivator implements BundleActivator {
+	
+	public void start(BundleContext context) throws Exception {
+		ProtegeProperties.getInstance().put("org.protege.differencecategory", "Ontology Comparison");
+	}
+
+	
+	public void stop(BundleContext context) throws Exception {
+	}
+}
