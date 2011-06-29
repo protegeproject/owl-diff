119569d5287f6c43e29f851a0d2ef9bef1d876a1
Timothy Redmond <tredmond@stanford.edu>
2011-06-29 18:32:00 +0000

    refactored into its own class because it is going to be useful

---
+++ src/main/java/org/protege/editor/owl/diff/ui/EntityBasedDiffRenderer.java
@@ -0,0 +1,31 @@
+package org.protege.editor.owl.diff.ui;
+
+import java.awt.Component;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import org.protege.owl.diff.present.EntityBasedDiff;
+import org.protege.owl.diff.service.RenderingService;
+
+public class EntityBasedDiffRenderer extends DefaultListCellRenderer {
+	private static final long serialVersionUID = -2257588249282053158L;
+	private RenderingService renderer;
+	
+	public EntityBasedDiffRenderer(RenderingService renderer) {
+		this.renderer = renderer;
+	}
+
+	@Override
+	public Component getListCellRendererComponent(JList list, Object value,
+												  int index, boolean isSelected, boolean cellHasFocus) {
+		JLabel text = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+		if (value instanceof EntityBasedDiff) {
+			EntityBasedDiff diff = (EntityBasedDiff) value;
+			text.setText(renderer.renderDiff(diff));
+		}
+		return this;
+	}
+	
+}
